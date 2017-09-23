9a140f550fe05c1b1907c340ebaa75b05196b64d
Andrea Suozzo <asuozzo@gmail.com>
2017-09-23 14:41:03 -0400

    separated data and added map api error handling function

--- js/script.js
+++ js/script.js
@@ -1,94 +1,3 @@
-var mapItems = [
-    {
-        "title": "Tapna Yoga",
-        "artist":"Clark Derbes",
-        "shortname":"tapna",
-        "description":"This new Burlington mural was funded by a $3,000 grant from Burlington City Arts.",
-        "imgSrc": "https://media1.fdncms.com/sevendaysvt/imager/u/blog/7674183/sota2-1-858bda70d3fcfbfd.jpg",
-        "geo": {
-            "lat": 44.471659,
-            "lng": -73.215599
-        }
-    },
-    {
-        "title": "Milton Gardener's Supply Warehouse",
-        "artist":"Sarah C. Rutherford",
-        "shortname":"gardenersupply",
-        "description":"The Milton Artists' Guild is on a mission to beautify the small northern Vermont town.",
-        "imgSrc": "https://media1.fdncms.com/sevendaysvt/imager/u/blog/7674184/sota2-2-4d7526841ba5632a.jpg",
-        "geo": {
-            "lat": 44.599052,
-            "lng": -73.164227
-        }
-    },
-    {
-        "title":"Dealer.com Silos",
-        "artist":"Mary Lacy",
-        "shortname":"dealersilos",
-        "description":"These urban silos sport the recognizable geometric stylings of Vermont artist Mary Lacy.",
-        "imgSrc": "https://media1.fdncms.com/sevendaysvt/imager/u/slideshow/2869652/episode412.jpg",
-        "geo": {
-            "lat": 44.465498,
-            "lng": -73.214417
-        }
-    },
-    {
-        "title":"Simon's Downtown Mobil",
-        "artist":"Anthill Collective",
-        "shortname":"simonmobil",
-        "description":"It took a few tries to get the right mural for this spot.",
-        "imgSrc":"https://media1.fdncms.com/sevendaysvt/imager/u/big/2933578/visualart1-1-7024f7f783c4c1f1.jpg",
-        "geo":{
-            "lat": 44.477873,
-            "lng": -73.211210
-        }
-    },
-    {
-        "title":"Jeffersonville Silos",
-        "artist":"Sarah C. Rutherford",
-        "shortname":"jeffsilos",
-        "description":"These massive murals stand out on two silos that once belonged to the Bell-Gates Lumber Corporation.",
-        "imgSrc":"https://media2.fdncms.com/sevendaysvt/imager/u/blog/3549564/artnews1-1-4caa0ac1bde58ba6.jpg",
-        "geo":{
-            "lat": 44.647701,
-            "lng": -72.829485
-        }
-    },
-    {
-        "title":"\"Everyone Loves a Parade\", Church Street Marketplace",
-        "artist":"Pierre Hardy",
-        "shortname":"churchst",
-        "description":"Oddly enough, Edward James Olmos has a cameo in the 124-foot-long mural.",
-        "imgSrc":"https://media2.fdncms.com/sevendaysvt/imager/u/blog/3632860/wtf1-1-2157cc1e91458592.jpg",
-        "geo":{
-            "lat": 44.478640,
-            "lng": -73.212469
-        }
-    },
-    {
-        "title":"\"You Are Loved\"",
-        "artist":"Alex Cook",
-        "shortname":"cherrystlove",
-        "description":"The Boston-based artist has painted more than 120 murals bearing this positive slogan.",
-        "imgSrc":"https://media1.fdncms.com/sevendaysvt/imager/u/blog/6212172/file_jun_12_12_44_57_pm.jpeg",
-        "geo":{
-            "lat": 44.479312,
-            "lng": -73.212308
-        }
-    },
-    {
-        "title":"Hummingbird",
-        "artist":"Mary Lacy",
-        "shortname":"hummingbird",
-        "description":"This ruby-throated hummingbird decorates a wall right next to City Hall Park.",
-        "imgSrc":"https://media1.fdncms.com/sevendaysvt/imager/mary-lacy/u/big/2613588/visualarts1-1.jpg",
-        "geo":{
-            "lat": 44.476979,
-            "lng": -73.214278
-        }
-    }
-];
-
 var map,
     infoWindow,
     bounds;
@@ -105,6 +14,10 @@ var initMap = function(){
 
     ko.applyBindings(new ViewModel());
 };
+
+var mapError = function(){
+    console.log("Sorry, something went wrong. Please try reloading the page.")
+}
 
 
 var MapItem = function(data) {
@@ -167,7 +80,7 @@ var MapItem = function(data) {
         keywords = keywords.toLowerCase();
 
         var match = true;
-        for (i=0;i<textList.length;i++){
+        for (var i=0;i<textList.length;i++){
             if (!keywords.includes(textList[i])){
                 match = false;
             }
@@ -282,7 +195,7 @@ var ViewModel = function(data){
                 response=response.response.groups[0].items;
 
                 var formattedResponse = "<ul>";
-                for (i=0;i<5;i++){
+                for (var i=0;i<5;i++){
                     var item = "<li><span><a href='https://foursquare.com/v/a/" + response[i].venue.id + "' target='_blank'>" + response[i].venue.name + "</a></span></li>";
                     formattedResponse += item;
                 }
